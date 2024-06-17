f827be0cd281d8c5d0173822a8b0d925c3a4d116
linhtql <tqlinh.na@gmail.com>
2024-06-17 17:03:30 +0700

    645. Set Mismatch

--- src/main/java/algorithms/SetMismatch.java
+++ src/main/java/algorithms/SetMismatch.java
@@ -1,13 +1,26 @@
 package algorithms;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SetMismatch {
     public static int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                return new int[]{nums[i], nums[i] + 1};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int missNum = 0;
+        int dupNum = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            if (!map.containsKey(i)) {
+                missNum = i;
+            } else {
+                if (map.get(i) == 2) {
+                    dupNum = i;
+                }
             }
         }
-        return new int[]{};
+        return new int[]{dupNum, missNum};
     }
 }
